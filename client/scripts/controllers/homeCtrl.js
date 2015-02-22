547ccae4a4caa8b060aef5f8d56fddd58c99d66d
mehdimj <mehdi.m.jamai@gmail.com>
2015-02-21 20:57:59 -0500

    HELLO TEAM

--- client/scripts/controllers/homeCtrl.js
+++ client/scripts/controllers/homeCtrl.js
@@ -14,23 +14,17 @@ gitStats.controller('homeCtrl', function($scope,$state,$http,$location){
     $scope.$watch(function () {
         return $location.search()
     }, function (value) {
-        console.log(value);
         $scope.CODE = value;
-
         //POST CODE TO AP
         if($scope.CODE) {
             $http.post('/api/session', $scope.CODE).
                 success(function (data, status, headers, config) {
-                    console.log(status);
                     $state.go('Dashboard');
                 }).
                 error(function (data, status, headers, config) {
                     console.log(status);
                 });
-            console.log($scope.CODE);
-
         }
-
         //GET REPOS
         $http.get('/api/repos').
             success(function (data, status, headers, config) {
@@ -41,49 +35,6 @@ gitStats.controller('homeCtrl', function($scope,$state,$http,$location){
             error(function (data, status, headers, config) {
                 console.log(status);
             });
-        console.log($scope.CODE);
-
     });
-
-
-    //$scope.$on('$locationChangeStart', function(event) {
-    //    if ( $StateProvider() == 'callback') {
-    //        $scope.CODE=  $scope.$location.search().code  Home?code=d0c08f10606008a57e80;
-    //        console.log($scope.CODE);
-    //
-    //    }
-    //});
-    //$scope.$on("$stateChangeSuccess", function () {
-    //    //if($location.search() !== "" ){
-    //    //    $scope.CODE=  $location.code;
-    //    console.log($scope.search());
-    //}
-    //});
-    //$scope.$watch(function() {
-    //    return $location.search();
-    //}, function(){
-    //    if($location.search().code !== "" ){
-    //        $scope.CODE=  $location.code;
-    //        console.log($scope.CODE);
-    //    }
-    //});
-    //GETTING CODE FROM CALLBACK
-    //$scope.CODE=  $scope.$location.search().code callback?code=1f633d6a30dd221e456c;
-
-
-
-    //private repos
-
-
-//$scope.gitLogin = function(){
-
-//    $http.get('https://github.com/login/oauth/authorize/a32040ef864339506ec5/'+$scope.redirectPath+'//').
-//        success(function(data, status, headers, config) {
-//            console.log(status);
-//        }).
-//        error(function(data, status, headers, config) {
-//            console.log(status);
-//        });
-//};
 
 });
