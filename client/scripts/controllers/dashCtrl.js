dfdadf45ca04367880bb3757603d4358d8daf012
mehdimj <mehdi.m.jamai@gmail.com>
2015-02-22 10:25:38 -0500

    no message

--- client/scripts/controllers/dashCtrl.js
+++ client/scripts/controllers/dashCtrl.js
@@ -3,6 +3,8 @@
  */
 
 gitStats.controller('dashCtrl', function($scope,$localStorage,$stateParams,$state,$http,logout){
+
+
     $scope.repos= $localStorage.myRepos;
     console.log($scope.repos);
 
@@ -128,40 +130,49 @@ gitStats.controller('dashCtrl', function($scope,$localStorage,$stateParams,$stat
         logout.logoutUser();
     };
 
+
+
     //Chart Stuff
 
-        // Load the Visualization API and the piechart package.
-    google.load('visualization', '1.0', {'packages':['corechart']});
+    // Get the context of the canvas element we want to select
 
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
 
-    // Callback that creates and populates a data table,
-    // instantiates the pie chart, passes in the data and
-    // draws it.
-    function drawChart() {
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'Time');
-        data.addColumn('number', 'Value');
-        data.addRows([
-            //['Mushrooms', 3],
-            //['Onions', 1],
-            //['Olives', 1],
-            //['Zucchini', 1],
-            //['Pepperoni', 2]
-        ]);
 
-        // Set chart options
-        var options = {'title':'GitStats',
-            'width':400,
-            'height':300};
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-    }
+    //
+    //    // Load the Visualization API and the piechart package.
+    //google.load('visualization', '1.0', {'packages':['corechart']});
+    //
+    //// Set a callback to run when the Google Visualization API is loaded.
+    //google.setOnLoadCallback(drawChart);
+    //
+    //// Callback that creates and populates a data table,
+    //// instantiates the pie chart, passes in the data and
+    //// draws it.
+    //function drawChart() {
+    //
+    //    // Create the data table.
+    //    var data = new google.visualization.DataTable();
+    //    data.addColumn('number', 'Time');
+    //    data.addColumn('number', 'Value');
+    //    data.addRows([
+    //        //['Mushrooms', 3],
+    //        //['Onions', 1],
+    //        //['Olives', 1],
+    //        //['Zucchini', 1],
+    //        //['Pepperoni', 2]
+    //    ]);
+    //
+    //    // Set chart options
+    //    var options = {'title':'GitStats',
+    //        'width':400,
+    //        'height':300};
+    //
+    //    // Instantiate and draw our chart, passing in some options.
+    //    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    //    chart.draw(data, options);
+    //}
     //Computation
 
 
