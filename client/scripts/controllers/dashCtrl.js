3605f81eaad1446d266a03b30f60e37ce0cb122c
mehdimj <mehdi.m.jamai@gmail.com>
2015-02-21 23:56:46 -0500

    no message

--- client/scripts/controllers/dashCtrl.js
+++ client/scripts/controllers/dashCtrl.js
@@ -2,13 +2,13 @@
  * Created by Mehdi on 2015-02-21.
  */
 
-gitStats.controller('dashCtrl', function($scope,$localStorage,logoutServ){
+gitStats.controller('dashCtrl', function($scope,$localStorage){
     $scope.repos= $localStorage.myRepos;
     console.log($scope.repos);
 
-    $scope.logout = function(){
-        logoutServ.logoutUser()
-    }
+    //$scope.logout = function(){
+    //    logoutServ.logoutUser()
+    //}
 
 });
 
